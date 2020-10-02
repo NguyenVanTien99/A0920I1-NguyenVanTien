a13b19c82f416446c076a812f9fdf728aeb7368a
TienNguyen <tienvng99@gmail.com>
2020-10-02 14:55:04 +0700

    bai tap va thuc hanh 6,7 javascript

---
+++ module1/7. Biến, kiểu dữ liệu, toán tử/Bai_Tap/Ungdungchuyendoitien/script.js
@@ -0,0 +1,31 @@
+const money = document.getElementById('money')
+const currency1 = document.getElementById('type1')
+const currency2 = document.getElementById('type2')
+const btnConvert = document.getElementById("convertbt");
+const convert = document.getElementById('convert')
+
+btnConvert.addEventListener("click",convertMoney)
+
+function convertMoney(){
+    if(currency1.value === 'VND' && currency2.value === 'USD'){
+        convert.innerHTML = money.value/23000 + 'USD';
+    }else if(currency1.value === 'USD' && currency2.value === 'VND'){
+        convert.innerHTML = money.value*23000 + 'VND';
+    }else if(currency1.value === 'VND' && currency2.value === 'VND'){
+        convert.innerHTML = 'không hợp lệ'
+    }else{
+        convert.innerHTML = 'không hợp lệ'
+    }
+
+    console.log(currency1.value);
+    console.log(currency2.value);
+    console.log(typeof currency1.value);
+    console.log(typeof currency2.value);
+    console.log(currency1.value === 'VND');
+    console.log(currency2.value === 'USD');
+    console.log((currency1.value == 'VND' && currency2.value == 'USD'));
+    console.log((currency1.value == 'USD' && currency2.value == 'VND'))
+}
+
+
+
---
+++ module1/7. Biến, kiểu dữ liệu, toán tử/Bai_Tap/maytinh/script.js
@@ -0,0 +1,34 @@
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const add = document.getElementById('add');
+const mul = document.getElementById('mul');
+const sub = document.getElementById('sub');
+const div = document.getElementById('div');
+const result = document.getElementById('result');
+
+
+
+add.addEventListener("click", addition);
+mul.addEventListener("click", multiplication);
+sub.addEventListener("click", Subtraction);
+div.addEventListener("click", division);
+
+function addition() {
+     result.innerHTML = 'result addition:' + (parseInt(num1.value) + parseInt(num2.value))
+}
+
+
+function multiplication() {
+     result.innerHTML = 'result multiplication' + (parseInt(num1.value) * parseInt(num2.value))
+}
+
+
+function Subtraction() {
+    
+    result.innerHTML = 'result Subtraction:' + (parseInt(num1.value) - parseInt(num2.value))
+}
+
+
+function division() {
+    result.innerHTML = 'result division:' + (parseInt(num1.value) / parseInt(num2.value))
+}

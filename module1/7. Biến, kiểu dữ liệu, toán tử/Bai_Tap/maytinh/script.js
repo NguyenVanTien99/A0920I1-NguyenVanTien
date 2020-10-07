b661001b2ec59080b2cf96e54d4be35d10e5af46
TienNguyen <tienvng99@gmail.com>
2020-10-07 17:44:10 +0700

    use one function

--- module1/7. Biến, kiểu dữ liệu, toán tử/Bai_Tap/maytinh/script.js
+++ module1/7. Biến, kiểu dữ liệu, toán tử/Bai_Tap/maytinh/script.js
@@ -5,30 +5,30 @@ const mul = document.getElementById('mul');
 const sub = document.getElementById('sub');
 const div = document.getElementById('div');
 const result = document.getElementById('result');
+// use 4 function
+
+// add.addEventListener("click", addition);
+// mul.addEventListener("click", multiplication);
+// sub.addEventListener("click", Subtraction);
+// div.addEventListener("click", division);
+
+// function addition() {
+//      result.innerHTML = 'result addition:' + (parseInt(num1.value) + parseInt(num2.value)) // }
 
 
-
-add.addEventListener("click", addition);
-mul.addEventListener("click", multiplication);
-sub.addEventListener("click", Subtraction);
-div.addEventListener("click", division);
-
-function addition() {
-     result.innerHTML = 'result addition:' + (parseInt(num1.value) + parseInt(num2.value))
-}
+// function multiplication() {
+//      result.innerHTML = 'result multiplication' + (parseInt(num1.value) * parseInt(num2.value))
+// }
 
 
-function multiplication() {
-     result.innerHTML = 'result multiplication' + (parseInt(num1.value) * parseInt(num2.value))
-}
-
-
-function Subtraction() {
+// function Subtraction() {
     
-    result.innerHTML = 'result Subtraction:' + (parseInt(num1.value) - parseInt(num2.value))
-}
+//     result.innerHTML = 'result Subtraction:' + (parseInt(num1.value) - parseInt(num2.value))
+// }
 
 
-function division() {
-    result.innerHTML = 'result division:' + (parseInt(num1.value) / parseInt(num2.value))
-}
+// function division() {
+//     result.innerHTML = 'result division:' + (parseInt(num1.value) / parseInt(num2.value))
+// }
+
+

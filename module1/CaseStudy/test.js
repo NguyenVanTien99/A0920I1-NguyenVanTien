cc7c3f3c815b9865599fa07dd500040a9f10ca9b
TienNguyen <tienvng99@gmail.com>
2020-11-23 16:05:31 +0700

    module2

--- module1/CaseStudy/test.js
+++ module1/CaseStudy/test.js
@@ -1,1 +1,22 @@
-console.log("jkhajsdh");
+function timeConversion(s) {
+    const arr = s.slice(0,8).split(':');
+    // arr[0] = (s.indexOf('PM') > -1) ?
+    //          (arr[0] == 12 ? '12' : Number(arr[0]) + 12) :
+    //          (arr[0] == 12 ? '00' : arr[0]);
+    // return arr.join(':');
+    if((s.indexOf('PM') > -1)){
+        if(arr[0] == 12){
+            arr[0] = '12'
+        }else{
+            arr[0] = Number(arr[0]) + 12)
+        }
+    }else{
+        if(arr[0] == 12){
+            arr[0] = '00'
+        }else{
+            arr[0]=arr[0]
+        }
+    }
+        return arr.join(':');
+    }
+
---
+++ module2/src/introduction_java/thuc_hanh/DeclareAndUseVariable.java
@@ -0,0 +1,12 @@
+package introduction_java.thuc_hanh;
+
+public class DeclareAndUseVariable {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "HaNoi";
+    }
+}
